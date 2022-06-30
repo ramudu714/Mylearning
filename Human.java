6019ce1b49e4ffdba9a53d789496d1300957f8ac
ramudu714 <ramudu714@gmail.com>
2022-06-30 17:57:43 +0530

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,10 @@
+
+public class Human {
+	   private String name;
+	   private String age;
+	   private Address address;
+
+	   private String getFullAddress() {
+	       return address.getFullAddress();
+	   }
+}
---
+++ Singleton.java
@@ -0,0 +1,21 @@
+
+public class Singleton {
+
+
+		
+		
+		private static Singleton obj;
+		 
+	    // private constructor to force use of
+	    // getInstance() to create Singleton object
+	    private Singleton() {}
+	 
+	    public static Singleton getInstance()
+	    {
+	        if (obj==null)
+	            obj = new Singleton();
+	        return obj;
+	    }
+	}
+
+
